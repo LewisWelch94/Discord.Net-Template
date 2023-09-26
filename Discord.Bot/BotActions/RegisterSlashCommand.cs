ff44604eb88204626addad4e9ce6381bc6db64b7
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2023-09-26 20:10:07 +0100

    now registers user commands

--- Discord.Bot/BotActions/RegisterSlashCommand.cs
+++ Discord.Bot/BotActions/RegisterSlashCommand.cs
@@ -1,6 +1,7 @@
 ﻿namespace Discord.Bot.BotActions;
 
 using Discord.Bot.SlashCommands;
+using Discord.Bot.UserCommands;
 using Discord.Net;
 using Discord.WebSocket;
 using Newtonsoft.Json;
@@ -10,15 +11,32 @@ public static class RegisterSlashCommand
 {
     public static async Task RegisterCommands(DiscordSocketClient client)
     {
-        var list = GetCommands();
+        var slashCommands = GetSlashCommands();
+        var userCommands = GetUserCommands();
 
-        foreach (var command in list)
+        foreach (var command in slashCommands)
         {
             var newCommand = command.Build();
             try
             {
                 await client.CreateGlobalApplicationCommandAsync(newCommand);
-                Console.WriteLine($"{newCommand.Name} has been registered");
+                Console.WriteLine($"{newCommand.Name} Slash command has been registered");
+            }
+            catch (HttpException e)
+            {
+                var json = JsonConvert.SerializeObject(e.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+                throw;
+            }
+        }
+
+        foreach (var command in userCommands)
+        {
+            var newCommand = command.Build();
+            try
+            {
+                await client.CreateGlobalApplicationCommandAsync(newCommand);
+                Console.WriteLine($"{newCommand.Name} User command has been registered");
             }
             catch (HttpException e)
             {
@@ -29,11 +47,19 @@ public static class RegisterSlashCommand
         }
     }
 
-    private static List<SlashCommandBuilder> GetCommands()
+    private static List<SlashCommandBuilder> GetSlashCommands()
     {
         return new List<SlashCommandBuilder>()
         {
             new TestSlashCommand().CreateSlashCommand(),
+        };
+    }
+
+    private static List<UserCommandBuilder> GetUserCommands()
+    {
+        return new List<UserCommandBuilder>
+        {
+            new TestUserCommand().CreateUserCommand(),
         };
     }
 }
