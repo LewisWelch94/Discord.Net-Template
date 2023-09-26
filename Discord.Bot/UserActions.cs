daf86a2c395c76c18cf1b39aee235a9dec37afa0
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2023-09-26 16:17:38 +0100

    Cleaned up user message

--- Discord.Bot/UserActions.cs
+++ Discord.Bot/UserActions.cs
@@ -7,25 +7,25 @@ public class UserActions : IUserActions
 {
     public async Task UserBanned(UserBannedNotification notification)
     {
-        var channel = notification.Guild.GetTextChannel(1090238156261036092);
-        await channel.SendMessageAsync($"${notification.User.Username} has been banned!");
+        var channel = notification.Guild.GetTextChannel(1111219523056574524) as ITextChannel;
+        await channel.SendMessageAsync($"{notification.User.Username} has been banned!");
     }
 
     public async Task UserJoined(UserJoinNotification notification)
     {
-        var channel = notification.Client.GetChannel(1090238156261036092) as ITextChannel;
-        await channel.SendMessageAsync($"${notification.User.Username} has joined!");
+        var channel = notification.Client.GetChannel(1111219523056574524) as ITextChannel;
+        await channel.SendMessageAsync($"{notification.User.Username} has joined!");
     }
 
     public async Task UserLeft(UserLeftNotification notification)
     {
-        var channel = notification.Guild.GetTextChannel(1090238156261036092);
-        await channel.SendMessageAsync($"${notification.User.Username} has left!");
+        var channel = notification.Guild.GetTextChannel(1111219523056574524) as ITextChannel;
+        await channel.SendMessageAsync($"{notification.User.Username} has left!");
     }
 
     public async Task UserUnBanned(UserUnBannedNotification notification)
     {
-        var channel = notification.Guild.GetTextChannel(1090238156261036092);
-        await channel.SendMessageAsync($"${notification.User.Username} has been unbanned!");
+        var channel = notification.Guild.GetTextChannel(1111219523056574524) as ITextChannel;
+        await channel.SendMessageAsync($"{notification.User.Username} has been unbanned!");
     }
 }
