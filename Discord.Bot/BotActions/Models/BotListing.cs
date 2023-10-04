22750412b34220a37d4142d7762e394a7efd8d1f
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2023-10-04 05:26:11 +0100

    Create BotListing.cs

---
+++ Discord.Bot/BotActions/Models/BotListing.cs
@@ -0,0 +1,12 @@
+﻿namespace Discord.Bot.BotActions.Models;
+
+public class BotListing
+{
+    public List<BotInfo> BotInfos { get; set; } = new List<BotInfo>();
+
+    public class BotInfo
+    {
+        public bool LastUsedInSoloGuildMode { get; set; }
+        public ulong? GuildId { get; set; }
+    }
+}
