729b63c12fafc112d3a8a8154bd92cc257d5f6b0
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2023-10-04 05:26:03 +0100

    Added CacheHelper

---
+++ Discord.Bot/BotActions/Helpers/CacheHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace Discord.Bot.BotActions.Helpers;
+
+using Discord.Bot.BotActions.Interfaces;
+using JsonFlatFileDataStore;
+
+public class CacheHelper : ICacheHelper
+{
+    public T GetCache<T>(string key, string file) where T : new()
+    {
+        using (var store = new DataStore(file))
+        {
+            try
+            {
+                return store.GetItem<T>(key);
+            }
+            catch
+            {
+                return new T();
+            }
+        }
+    }
+
+    public async Task SaveCache<T>(T cache, string key, string file) where T : new()
+    {
+        using (var store = new DataStore(file))
+        {
+            await store.ReplaceItemAsync(key, cache, true);
+        }
+    }
+}
---
+++ Discord.Bot/BotActions/Interfaces/ICacheHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace Discord.Bot.BotActions.Interfaces;
+
+public interface ICacheHelper
+{
+    T GetCache<T>(string key, string file) where T : new();
+
+    Task SaveCache<T>(T cache, string key, string file) where T : new();
+}
