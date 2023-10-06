8f512345dfa213aab4a42f51f4b282cb1938c33e
Lewisaaronwelch <48370571+LewisWelch94@users.noreply.github.com>
2023-10-06 21:00:59 +0100

    Added UserHandler

---
+++ Discord.Bot/BotActions/Handlers/UserHandlers/UserUpdatedHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Discord.Bot.BotActions.Handlers.UserHandlers;
+
+using Discord.Bot.BotActions.Notifications.UserNotifications;
+using MediatR;
+
+internal class UserUpdatedHandler : INotificationHandler<UserUpdatedNotification>
+{
+    public async Task Handle(UserUpdatedNotification notification, CancellationToken cancellationToken)
+    {
+        var channel = await notification.Client.GetChannelAsync(1111219523056574524) as ITextChannel;
+        await channel.SendMessageAsync($"User has been updated\n\n Old: ${notification.PreviousUser}\n\nNew: ${notification.CurrentUser}");
+    }
+}
